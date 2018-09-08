7fc64838544addcd04e40a55062a186011f588be
Sai Haridass <sharidass1408@gmail.com>
2018-09-08 10:07:30 -0400

    commiting from vs

--- Open Editor/app.js
+++ Open Editor/app.js
@@ -3,6 +3,10 @@ var name = "Srija";
 console.log(name +' :is a type of ' + typeof name);
 
 var age = 45;
-console.log(age + ' is type of' + typeof age);
+console.log(age + ' :is a type of ' + typeof age);
+
+var car = null;
+console.log(car + ' :is a type of ' + typeof car);
+
 
 
