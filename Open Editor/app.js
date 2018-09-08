1f168c3224a2b0c0773e3380b906b28cf57541a9
Sai Haridass <sharidass1408@gmail.com>
2018-09-08 10:05:33 -0400

    Commiting from VisualStudio

--- Open Editor/app.js
+++ Open Editor/app.js
@@ -1,6 +1,6 @@
 
 var name = "Srija";
-console.log(name +' is type of' + typeof name);
+console.log(name +' :is a type of ' + typeof name);
 
 var age = 45;
 console.log(age + ' is type of' + typeof age);
