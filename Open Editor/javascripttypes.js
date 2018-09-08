0da965e62639de24bc6f8f66b0032f8daef848d5
Sai Haridass <sharidass1408@gmail.com>
2018-09-08 12:16:23 -0400

    javascript Types

---
+++ Open Editor/javascripttypes.js
@@ -0,0 +1,40 @@
+
+var name = "Srija";
+console.log('name  :is a type of ' + typeof name);
+
+var age = 45;
+console.log('age :is a type of ' + typeof age);
+
+var car = null;
+console.log('null :is a type of ' + typeof car);
+
+var purpose;
+console.log('undefined :is a type of ' + typeof purpose);
+
+var isHappyLife = true;
+console.log('isHappyLife :is a type of ' + typeof  isHappyLife);
+
+//Symbol
+const sym = Symbol();
+console.log('Symbol :is a type of ' + typeof sym);
+
+//Array
+const hobbies = ['movies', 'music'];
+console.log('hobbies  Array :is a type of ' + typeof hobbies);
+
+//Object literal
+const address = {
+    city: 'Boston',
+    state: 'MA'
+}
+console.log('address  :is a type of ' + typeof address);
+
+const today = new Date();
+console.log('today is ' + today + ' and is of type ' + typeof today);
+
+
+
+
+
+
+
