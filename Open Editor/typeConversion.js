fe769ec55160131928a35d10f64c6815d44f48a6
Sai Haridass <sharidass1408@gmail.com>
2018-09-16 20:15:32 -0400

    TypeConversion

---
+++ Open Editor/typeConversion.js
@@ -0,0 +1,21 @@
+let val;
+
+val = 5;
+
+console.log(val);
+console.log(typeof val);
+console.log(val.length);
+
+//Number to String
+
+val = String(5);
+console.log(val.length);
+
+
+//Bool to String
+val = String(true);
+console.log(val.length);
+
+val = String([1,2,3,1])
+console.log(val.length);
+
